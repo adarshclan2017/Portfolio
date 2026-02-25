99adc10da4c8be674fe91917733eda6fe593b4dc
ADARSH <adarshclan2017@gmail.com>
2026-02-25 13:07:17 +0530

    feat:api commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const authApi = () => {
   const token = localStorage.getItem("admin_token");
+
   return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: `${BASE_URL}/api`,
     headers: token ? { Authorization: `Bearer ${token}` } : {},
   });
 };
